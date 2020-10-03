432c55da5c48ffaa97a6d70aa1cad69170f0e4dc
Quang Tín <63833652+quangtin131299@users.noreply.github.com>
2020-10-03 11:21:46 +0800

    first commit

--- app/src/main/java/com/example/appdatvexemphim/MainActivity.java
+++ app/src/main/java/com/example/appdatvexemphim/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.appdatvexemphim;
 
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.RotatingCircle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.progress);
+        Sprite doubleBounce = new RotatingCircle();
+        progressBar.setIndeterminateDrawable(doubleBounce);
     }
 }
