2409b032b5de4146aea0386a54a49e7a9e7f8620
Quang Tín <63833652+quangtin131299@users.noreply.github.com>
2020-10-07 21:55:36 +0800

    quang tin cap nhat ngay 07/10/2020 21:55

--- app/src/main/java/com/example/appdatvexemphim/Activity/HomeActivity.java
+++ app/src/main/java/com/example/appdatvexemphim/Activity/HomeActivity.java
@@ -1,16 +1,64 @@
 package com.example.appdatvexemphim.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.appdatvexemphim.Adapter.MainAdapter;
+import com.example.appdatvexemphim.Fragment.FragmentCommingSoon;
+import com.example.appdatvexemphim.Fragment.FragmentNearYou;
+import com.example.appdatvexemphim.Fragment.FragmentPremieres;
 import com.example.appdatvexemphim.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ArrayList<String> arrayTilte = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        addControl();
+        prepearViewPager(viewPager, arrayTilte);
+        setTabLayoutWithViewPager();
     }
+
+    private void setTabLayoutWithViewPager() {
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+    private void addControl() {
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        arrayTilte.add("Near You");
+        arrayTilte.add("Comming soon");
+        arrayTilte.add("Premieres");
+    }
+
+    private void prepearViewPager(ViewPager viewPager, ArrayList<String> arrayTilte) {
+        MainAdapter mainAdapter = new MainAdapter(getSupportFragmentManager());
+
+        FragmentNearYou fragmentNearYou = new FragmentNearYou();
+        FragmentCommingSoon fragmentCommingSoon = new FragmentCommingSoon();
+        FragmentPremieres fragmentPremieres = new FragmentPremieres();
+
+        mainAdapter.addFragment(fragmentNearYou, arrayTilte.get(0));
+        mainAdapter.addFragment(fragmentCommingSoon, arrayTilte.get(1));
+        mainAdapter.addFragment(fragmentPremieres, arrayTilte.get(2));
+
+        viewPager.setAdapter(mainAdapter);
+
+    }
+
 }
---
+++ app/src/main/java/com/example/appdatvexemphim/Adapter/ListMovieAdapter.java
@@ -0,0 +1,29 @@
+package com.example.appdatvexemphim.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.appdatvexemphim.R;
+
+public class ListMovieAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.linerecylerview_datamovienearyou, parent, false);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
